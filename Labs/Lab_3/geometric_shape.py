d889a3741c6e6134e6554a14d191d130cc8356e2
Wessnom <wessneralexander@gmail.com>
2023-10-15 12:26:55 +0200

    lagt till testfilen och fler metoder

--- Labs/Lab_3/geometric_shape.py
+++ Labs/Lab_3/geometric_shape.py
@@ -1,12 +1,11 @@
 from math import pi
 # from math import sqrt // for triangles
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
 
 class Geometric_shape:
+    '''Represents a general geometric shape with a center position (x, y).'''
+
     def __init__(self, x, y):
+        '''Initializes a geometric shape with a given center position.'''
         self.x = x
         self.y = y
     
@@ -46,23 +45,58 @@ class Geometric_shape:
     def __str__(self):
         raise NotImplementedError
     
-    def transpose(self, x, y):
-        raise NotImplementedError
+    def transpose(self, dx, dy): # Inkludera felhantering
+        self.x += dx
+        self.y += dy
 
     def is_inside(self, x, y):
+        '''Checks if a point (x, y) is inside the geometric shape or on the perimeter.'''
         raise NotImplementedError
         
         
+class Circle(Geometric_shape):
+    '''Represents a circle with a center position and a radius.'''
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+
+    @property
+    def area(self):
+        return pi * self.radius ** 2
+    
+    @property
+    def circumference(self):
+        return 2 * pi * self.radius
+    
+    def is_inside(self, x, y):
+        distance = ((x - self.x)**2 + (y - self.y)**2)**0.5  # calculate the distance from the point to the center of the circle
+        return distance <= self.radius  # return True if the distance is less than or equal to the radius, else return False
+
+    def is_unit(self):
+        return self.radius == 1
     
     
+class Rectangle(Geometric_shape):
+    '''Represents a rectangle with a center position and two sides (side1, side2).'''
+
+    def __init__(self, x, y, side1, side2):
+        super().__init__(x, y)
+        self.side1 = side1
+        self.side2 = side2
+
+    @property
+    def area(self):
+        return self.side1 * self.side2
+
+    @property
+    def circumference(self):
+        return 2 * (self.side1 + self.side2)
     
-class Square(Geometric_shape):
-    NotImplementedError
+    def is_inside(self, x, y):
+        half_width = self.side1 / 2
+        half_height = self.side2 /2
+        return self.x - half_width <= x <= self.x + half_width and self.y - half_height <= y <= self.y + half_height
     
-    
-    
-    
-    
-class Cirkle(Geometric_shape):
-    NotImplementedError
-    
+    def is_square(self):
+        return self.side1 == self.side2
